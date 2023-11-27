f8b6e0f57a66a11c05ec4e57f499e73f6b133bf8
Rekha Bhaskar <rekha6bhaskar@gmail.com>
2023-11-27 15:31:16 +0530

    files loaded

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,43 +1,78 @@
-# import libraries 
-import pandas as pd
-import streamlit as st 
-import pickle 
-from pathlib import Path
 
-load = pickle.load(open('lr_classifier.sav',"rb"))
+import streamlit as st
+import joblib
+import warnings
 
-vectorize = pickle.load(open('lr_vectorizer.sav', "rb"))
+warnings.filterwarnings('ignore')
 
-def news_prediction(news):
-  # new_news = "LONDON (Reuters) - LexisNexis, a provider of l."
-  testing_news = {"text":[news]}
-  new_def_test = pd.DataFrame(testing_news)
-  new_x_test = new_def_test['text']
-  new_tfidf_test = vectorize.transform(new_x_test)
-  pred_dt = load.predict(new_tfidf_test)
-  
-  if (pred_dt[0] == 0):
-    return "This is Fake News!"
-  else:
-    return "The News seems to be True!"
+ 
+import numpy as np  # linear algebra   
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv) 
+
+# df1 = pd.read_csv("True.csv", sep=',', encoding='ISO-8859-1', skiprows=lambda x: x in [11067])
+# df2 = pd.read_csv("Fake.csv", sep=',', encoding='ISO-8859-1', skiprows=lambda x: x in [11067]) 
+
+# try:
+#     df1 = pd.read_csv("True.csv", sep=',', encoding='ISO-8859-1', skiprows=lambda x: x in [11067])
+#     df2 = pd.read_csv("Fake.csv", sep=',', encoding='ISO-8859-1', skiprows=lambda x: x in [11067])
+# except pd.errors.ParserError as e:
+#     print(f"Error reading CSV: {e}")
+#     # Handle the error as needed
+
+# df1 = pd.read_csv("True.csv", sep=',', encoding='ISO-8859-1', skiprows=lambda x: x in [11067])
+# df2 = pd.read_csv("Fake.csv", sep=',', encoding='ISO-8859-1', skiprows=lambda x: x in [11067]) 
+# data = pd.concat([df1, df2], ignore_index=True, sort=False)
+
+model = joblib.load('model.pkl')
+vectorizer = joblib.load('vectorizer.pkl') 
+
+
+def predict_news(input_text, vectorizer, model):
+    input_tfidf = vectorizer.transform([input_text])
+    prediction = model.predict(input_tfidf)[0]
+    return prediction
+
+
+# Streamlit app
+st.title("📰 Fake News Detection App")
+st.markdown(
+    """
+    <style>
+        body {
+            background-color: #f5f5f5;
+        }
+        .stButton button {
+            background-color: #4caf50;
+            color: white;
+        }
+        .stSelectbox {
+            width: 80%;
+        }
+        .stText {
+            font-size: 18px;
+            font-weight: bold;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+# Select a text from the loaded data
+# selected_text = st.selectbox("Select a text:", data['text'])
+selected_text = st.text_input('')
+
+# Display the selected text
+st.markdown("<p class='stText'>Selected Text:</p>", unsafe_allow_html=True)
+st.write(selected_text)
+
+# Button to make predictions
+if st.button("Predict"):
+    # Make a prediction
+    prediction = predict_news(selected_text, vectorizer, model)
+
+    # Display the prediction result
+    st.markdown(f"<p class='stText'>Prediction: {'Real' if prediction == 1 else 'Fake'}</p>", unsafe_allow_html=True)
+
 
 
     
-def main():
-  
-  st.title("Fake News Prediction System")
-  st.write("""## Input your News Article down below: """)
-  user_text = st.text_input('')
-  if st.button("Predict"):
-    news_pred = news_prediction(user_text)
-    
-    if (news_pred == "This is Fake News!"):
-      st.error(news_pred, icon="🚨")
-    else:
-      st.success(news_pred)
-      st.balloons()
-
-  
-if __name__ == "__main__":
-  main()
-  
